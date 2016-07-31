d200fa4b71335e223ae0341590f597d0bd63ad70
jakub.krolewski <jakub.krolewski@none>
2016-07-31 15:03:50 +0200

    Showing toolbar in webview activity

--- app/src/main/java/pl/jkrolewski/stacksearch/details/SimpleWebViewActivity.java
+++ app/src/main/java/pl/jkrolewski/stacksearch/details/SimpleWebViewActivity.java
@@ -5,15 +5,25 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
+import android.support.v7.widget.Toolbar;
 import android.webkit.WebView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import lombok.NonNull;
+import pl.jkrolewski.stacksearch.R;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class SimpleWebViewActivity extends AppCompatActivity {
 
-    private WebView webView;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.web_view)
+    WebView webView;
 
     public static Intent createIntent(@NonNull Context context, @NonNull String url) {
         return new Intent(context, SimpleWebViewActivity.class)
@@ -23,25 +33,22 @@ public class SimpleWebViewActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        webView = new WebView(this);
-        webView.loadUrl(getIntent().getDataString());
-//        webView.setWebViewClient(new WebViewClient() {
-//            @Override
-//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//                view.loadUrl(url);
-//                return true;
-//            }
-//        });
 
-        this.setContentView(webView);
+        setContentView(R.layout.simple_webview_activity);
+        ButterKnife.bind(this);
+
+        setUpToolbar();
+        loadContent();
     }
 
-    @Override
-    public boolean onKeyDown(final int keyCode, final KeyEvent event) {
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
-            webView.goBack();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
+    private void setUpToolbar() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = checkNotNull(getSupportActionBar());
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void loadContent() {
+        webView.loadUrl(getIntent().getDataString());
     }
 }
